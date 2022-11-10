e0401d8cf5c19393e667cb8dd29afc4e161ca1b0
MaxShvachko <maxshvachko@ukr.net>
2022-11-10 19:12:23 +0200

    Add delete post logic

--- src/entities/Updoot.ts
+++ src/entities/Updoot.ts
@@ -14,13 +14,13 @@ export class Updoot extends BaseEntity {
   userId!: number;
 
   @Field(() => User)
-  @ManyToOne(() => User, (user) => user.updoots)
+  @ManyToOne(() => User, (user) => user.updoots, { onDelete:'CASCADE' })
   user: User
 
   @PrimaryColumn()
   postId!: number;
 
   @Field(() => User)
-  @ManyToOne(() => Post, (post) => post.updoots)
+  @ManyToOne(() => Post, (post) => post.updoots, { onDelete:'CASCADE' })
   post: Post
 }
